16d845f09ad3f36ef868bd50c8650e7afae278ac
MATEUS COUTINHO MARIM <14382065771@lab05m38.nrc.ice.ufjf.br>
2015-04-24 17:05:58 -0300

    unuseless code deleted

--- src/Amostra.java
+++ src/Amostra.java
@@ -5,35 +5,29 @@ public class Amostra {
 	private final int nAm;	//Numero da amostra
 	
 	public Amostra(int nAm, int nAtrib){
-		this.nAm = nAm;
-		this.dados = new float[nAtrib];
+            this.nAm = nAm;
+            this.dados = new float[nAtrib];
 	}
 	
 	public void addVal(float val){
-			this.dados[atributo]=val;
-			atributo++;
+            this.dados[atributo]=val;
+            atributo++;
 	}
 	
-	public float getVal(int atrib){
-//		if(atrib >= 0 && atrib <= this.dados.length)	
-			return this.dados[atrib];
-//		else{
-//			System.out.println("Atributo fora do intervalo do Array.(1)");
-//			return -1;
-//		}
-	}
+	public float getVal(int atrib){	
+            return this.dados[atrib];
+        }
 	
 	public void chanVal(int index, float val){
-		for(int i = 0; i < dados.length; i++)
-			if(index == i)
-				dados[i] = val;	
+            for(int i = 0; i < dados.length; i++)
+		if(index == i)
+                    dados[i] = val;	
 	}
 	
 	public int getClasse(){
-		return this.classe;
+            return this.classe;
 	}
 	
 	public int getNumAmos(){
-		return nAm;
+            return nAm;
 	}
-}
